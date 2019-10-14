539551872fb5ab5b012b333a00ccd2ec82f65297
Benedict Pfender <benedict.pfender@gmail.com>
2019-10-14 15:09:46 +0100

    Update to roman to put double numeral handling in function

--- Lab 9/roman.c
+++ Lab 9/roman.c
@@ -1,63 +1,69 @@
+#include <assert.h>
+#include <ctype.h>
 #include <stdio.h>
 
+typedef enum bool { false,
+                    true } bool;
+
 int romanToArabic(char* roman);
+int numeralValue(char roman);
+int checkSecondNumeral(char numeral, char next_numeral, int* index);
 
 int main(int argc, char** argv) {
     if (argc == 2) {
         printf("The roman number %s is equal to %d\n",
                argv[1], romanToArabic(argv[1]));
     } else {
-        printf("ERROR: Incorrect usage, try e.g. %s XXI\n", argv[0]);
+        fprintf(stderr,
+                "ERROR: Incorrect usage, try e.g. %s XXI\n", argv[0]);
     }
 
     return 0;
 }
 
+int checkSecondNumeral(char numeral, char next_numeral, int* index) {
+    int multiplier;
+
+    switch (toupper(numeral)) {
+        case 'I':
+            multiplier = 1;
+            break;
+        case 'X':
+            multiplier = 10;
+            break;
+        case 'C':
+            multiplier = 100;
+            break;
+    }
+
+    switch (next_numeral) {
+        case 'V':
+        case 'L':
+        case 'D':
+            (*index)++;
+            return 4 * multiplier;
+        case 'X':
+        case 'C':
+        case 'M':
+            (*index)++;
+            return 9 * multiplier;
+        default:
+            return multiplier;
+    }
+}
+
 /* QUESTION can this be shortened? */
 int romanToArabic(char* roman) {
-    int i = 0;
+    int i;
     int sum = 0;
 
-    while (*(roman + i) != '\0') {
-        switch (roman[i]) {
-            case 'I':
-                if (roman[i + 1] == 'V') {
-                    sum += 4;
-                    i++;
-                } else if (roman[i + 1] == 'X') {
-                    sum += 9;
-                    i++;
-                } else {
-                    sum += 1;
-                }
-                break;
+    for (i = 0; roman[i] != '\0'; i++) {
+        switch (toupper(roman[i])) {
             case 'V':
                 sum += 5;
                 break;
-            case 'X':
-                if (roman[i + 1] == 'L') {
-                    sum += 40;
-                    i++;
-                } else if (roman[i + 1] == 'C') {
-                    sum += 90;
-                    i++;
-                } else {
-                    sum += 10;
-                }
-                break;
             case 'L':
                 sum += 50;
-                break;
-            case 'C':
-                if (roman[i + 1] == 'D') {
-                    sum += 400;
-                    i++;
-                } else if (roman[i + 1] == 'M') {
-                    sum += 900;
-                    i++;
-                } else {
-                    sum += 100;
-                }
                 break;
             case 'D':
                 sum += 500;
@@ -65,8 +71,35 @@ int romanToArabic(char* roman) {
             case 'M':
                 sum += 1000;
                 break;
+            default: /* Check for second numeral for I, X and C */
+                sum += checkSecondNumeral(roman[i], roman[i + 1], &i);
+                break;
         }
-        i++;
     }
     return sum;
+}
+
+int numeralValue(char roman) {
+    switch (roman) {
+        case 'M':
+            return 1000;
+        case 'D':
+            return 500;
+        case 'C':
+            return 100;
+        case 'L':
+            return 50;
+        case 'X':
+            return 10;
+        case 'V':
+            return 5;
+        case 'I':
+            return 1;
+        default:
+            fprintf(stderr, "%c is not a valid roman numeral\n", roman);
+            return 0;
+    }
+}
+
+void test(void) {
 }
