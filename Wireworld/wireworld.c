b5b4b272f33abf07219bf0cfed7747a53316eb40
Benedict Pfender <benedict.pfender@gmail.com>
2019-10-20 16:18:02 +0100

    Basic functionality complete. Needs padding, error checking and clean up

--- Wireworld/wireworld.c
+++ Wireworld/wireworld.c
@@ -5,25 +5,43 @@
 typedef enum bool { false,
                     true } bool;
 
-/* TODO, could define as 42 to ensure bounding cells or just add some padding? */
 #define CIRC_GRID 40
+#define PADDING 1
 #define MOORE_GRID 3
+#define GENERATIONS 5
 
-void loadCircuit(char* filename, char circuit[][CIRC_GRID]);
+bool loadCircuit(char* filename, char circuit[][CIRC_GRID]);
 char nextCellState(char state[][CIRC_GRID], int i, int j);
 void nextGeneration(char circuit[][CIRC_GRID]);
-int checkMooreNeighborhood(char* state);
+int checkMooreNeighborhood(char* state, int parent_grid_size);
 bool checkSymbol(char c);
 int getLine(char buffer[], int buffer_size, FILE* fp);
 void test(void);
+void printCircuit(char circuit[][CIRC_GRID]);
+bool checkCircuit(char circuit[][CIRC_GRID]);
 
 int main(void) {
+    char circuit[CIRC_GRID][CIRC_GRID];
+    int i;
+
     test();
+
+    if (loadCircuit("wirewcircuit1.txt", circuit) == false) {
+        return 1;
+    }
+    if (checkCircuit(circuit) == false) {
+        return 1;
+    }
+
+    for (i = 0; i < GENERATIONS; i++) {
+        nextGeneration(circuit);
+        printCircuit(circuit);
+    }
 
     return 0;
 }
 
-void loadCircuit(char* filename, char circuit[][CIRC_GRID]) {
+bool loadCircuit(char* filename, char circuit[][CIRC_GRID]) {
     FILE* file;
     char buffer[CIRC_GRID + 1];
     int length;
@@ -33,18 +51,27 @@ void loadCircuit(char* filename, char circuit[][CIRC_GRID]) {
     file = fopen(filename, "r");
     if (file == NULL) {
         fprintf(stderr, "Cannot open %s\n", filename);
-        exit(EXIT_FAILURE);
+        return false;
     }
 
     while (length = (getLine(buffer, CIRC_GRID + 1, file))) {
-        if (length != 40) {
+        if (length != CIRC_GRID) {
             fprintf(stderr,
-                    "Circuit width in line %d is not %d cells wide",
+                    "Circuit width in line %d is not %d cells wide\n",
                     i, CIRC_GRID);
-            exit(EXIT_FAILURE);
+            fclose(file);
+            return false;
+        }
+        if (i >= CIRC_GRID) {
+            fprintf(stderr,
+                    "Too many lines in input file\n");
+            fclose(file);
+            return false;
         }
 
-        printf("%s %d %d\n", buffer, i, j);
+        for (j = 0; j < CIRC_GRID; j++) {
+            circuit[i][j] = buffer[j];
+        }
         i++;
     }
 
@@ -81,7 +108,8 @@ bool checkSymbol(char c) {
 
 /* TODO unsure about this function... could it be better? plus error handling
  * what about fgets etc.
- * TODO could be rewritten to write directly into matrix
+ * TODO could be rewritten to write directly into matrix. 
+ * QUESTION as above, what is more elegant?
  */
 int getLine(char buffer[], int buffer_size, FILE* fp) {
     int c, i;
@@ -139,7 +167,7 @@ char nextCellState(char state[][CIRC_GRID], int i, int j) {
         case 'c':
             /* TODO need to check surrounding cells here */
             /* TODO how to handle larger to smaller 2D array? */
-            heads = 1;
+            heads = checkMooreNeighborhood(&state[i][j], CIRC_GRID);
             if (heads == 1 || heads == 2) {
                 return 'H';
             }
@@ -150,26 +178,41 @@ char nextCellState(char state[][CIRC_GRID], int i, int j) {
     }
 }
 
+void printCircuit(char circuit[][CIRC_GRID]) {
+    int i;
+
+    /* QUESTION is this  a naughty way of doing this? */
+    /* FIXME hard coded line bounds at the moment */
+    for (i = 4; i < 15; i++) {
+        printf("%.*s\n", CIRC_GRID, circuit[i]);
+    }
+}
+
 /* TODO need to check bounding is valid don't go out of index
  * QUESTION does c90 allow negative indexing? does the size in [] make any difference
  * QUESTION is it better to treat as contiguous memory or 2d array?
  * TODO, better to use whole CIRC_GRID for easier indexing, but harder testing
  * TODO currently expects pointer to center of 3x3 grid
  */
-int checkMooreNeighborhood(char* state) {
-    int i;
+/* Passing in parent grid size allows me to check circuit array directly and also test smaller matrices */
+/* QUESTION have written function with testing in mind */
+int checkMooreNeighborhood(char* state, int parent_grid_size) {
+    int i, j;
     int head_count = 0;
 
     /* TODO this is horrible at the moment.. needs to be updated */
-    for (i = -4; i < 5; i++) {
-        if (i != 0) {
-            head_count += (state[i] == 'H');
-        }
+    for (i = -1; i < 2; i++) {
+        for (j = -1; j < 2; j++)
+            if (!(i == 0 && j == 0)) {
+                head_count += (*(state + i + j * parent_grid_size) == 'H');
+            }
     }
     return head_count;
 }
 
 void test(void) {
+    int i;
+
     char moore_test_1[][MOORE_GRID] = {{' ', ' ', ' '},
                                        {'H', 'c', 'c'},
                                        {' ', ' ', ' '}};
@@ -188,11 +231,11 @@ void test(void) {
     char circuit[CIRC_GRID][CIRC_GRID];
 
     moore_state = &moore_test_1[1][1];
-    assert(checkMooreNeighborhood(moore_state) == 1);
+    assert(checkMooreNeighborhood(moore_state, MOORE_GRID) == 1);
     moore_state = &moore_test_2[1][1];
-    assert(checkMooreNeighborhood(moore_state) == 2);
+    assert(checkMooreNeighborhood(moore_state, MOORE_GRID) == 2);
     moore_state = &moore_test_3[1][1];
-    assert(checkMooreNeighborhood(moore_state) == 0);
+    assert(checkMooreNeighborhood(moore_state, MOORE_GRID) == 0);
 
     loadCircuit(filename, circuit);
 }
