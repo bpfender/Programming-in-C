b53db3a749bd5bc6be3ccc31cafabf864c9dba54
Benedict Pfender <benedict.pfender@gmail.com>
2019-11-04 00:18:47 +0000

    Maze finished. NOt massively pretty but functionally there

--- Week 5/maze.c
+++ Week 5/maze.c
@@ -4,31 +4,33 @@
 #include <string.h>
 
 typedef struct maze {
-    char** final_grid;
-    int route_length;
-    char** grid;
+    char** final;
+    int min_route;
+    char** test;
     int size_x;
     int size_y;
     int entrance_x;
     int entrance_y;
 } maze;
 
-int exploreMaze(maze* maze, int x, int y, int length);
-int createMazeArray(maze* maze);
-void freeMazeArray(maze* maze);
+void exploreMaze(maze* maze, int x, int y, int length);
+void savePath(maze* maze, int length);
+void findEntrance(maze* maze);
 int isExit(maze* maze, int x, int y);
+
 int loadMaze(char* filename, maze* maze);
 int readMazeSize(maze* maze, char* line);
+int initialiseMaze(maze* maze);
+char** createMazeArray(int size_x, int size_y);
+void freeMazeArray(maze* maze);
+
 int getLine(char** buffer, int* size, FILE* file);
-void findEntrance(maze* maze);
-int checkExit(maze* maze);
-void printMaze(maze* maze);
+void printSolution(maze* maze);
+
 void test(void);
 
 int main(int argc, char* argv[]) {
     maze maze;
-    int i, j;
-
     /* test();*/
 
     if (argc != 2) {
@@ -41,120 +43,73 @@ int main(int argc, char* argv[]) {
     if (!loadMaze(argv[1], &maze)) {
         return 1;
     }
-    printMaze(&maze);
-    findEntrance(&maze);
 
+    findEntrance(&maze);
     exploreMaze(&maze, maze.entrance_x, maze.entrance_y, 0);
 
-    for (i = 0; i < maze.size_y; i++) {
-        for (j = 0; j < maze.size_x; j++) {
-            printf("%c", maze.final_grid[i][j]);
-        }
-        printf("\n");
+    if (maze.min_route == -1) {
+        printf("No route through maze\n");
+    } else {
+        printf("Shortest route is %d steps\n", maze.min_route);
+        printSolution(&maze);
     }
 
     freeMazeArray(&maze);
     return 0;
 }
 
-int createMazeArray(maze* maze) {
-    int i;
-
-    maze->grid = (char**)malloc(maze->size_y * sizeof(char*));
-    maze->final_grid = (char**)malloc(maze->size_y * sizeof(char*));
-
-    if (maze->grid == NULL || maze->final_grid == NULL) {
-        fprintf(stderr, "Memory allocation error\n");
-        return 0;
-    }
-
-    for (i = 0; i < maze->size_y; i++) {
-        maze->grid[i] = (char*)malloc(maze->size_x * sizeof(char));
-        maze->final_grid[i] = (char*)malloc(maze->size_x * sizeof(char));
-        if (maze->grid[i] == NULL || maze->final_grid[i] == NULL) {
-            fprintf(stderr, "Memory allocation error\n");
-            return 0;
-        }
-    }
-
-    maze->route_length = -1;
-
-    return 1;
-}
-
-void freeMazeArray(maze* maze) {
-    int i;
-
-    for (i = 0; i < maze->size_y; i++) {
-        free(maze->grid[i]);
-        free(maze->final_grid[i]);
-    }
-    free(maze->grid);
-    free(maze->final_grid);
-}
-
-int isExit(maze* maze, int x, int y) {
-    if (y == 0 || y == maze->size_y - 1 || x == 0 || x == maze->size_x - 1) {
-        if (!(y == maze->entrance_y && x == maze->entrance_x)) {
-            return 1;
-        }
-    }
-    return 0;
-}
-
-int exploreMaze(maze* maze, int x, int y, int length) {
-    int i, j;
-    printf("Cood x:%d y:%d\n", x, y);
-    maze->grid[y][x] = '.';
+void exploreMaze(maze* maze, int x, int y, int length) {
+    maze->test[y][x] = '.';
 
     if (isExit(maze, x, y)) {
-        printf("Length %d x %d y %d\n", length, x, y);
-        if (maze->route_length < 0) {
-            maze->route_length = length;
-
-        } else if (length < maze->route_length) {
-            maze->route_length = length;
-        }
-
-        for (i = 0; i < maze->size_y; i++) {
-            for (j = 0; j < maze->size_y; j++) {
-                maze->final_grid[i][j] = maze->grid[i][j];
-            }
-        }
-
-        return 1;
+        savePath(maze, length);
+        return;
     }
 
-    if (y > 0 && maze->grid[y - 1][x] == ' ') {
+    if (y > 0 && maze->test[y - 1][x] == ' ') {
         exploreMaze(maze, x, y - 1, length + 1);
-        maze->grid[y - 1][x] = ' ';
+        maze->test[y - 1][x] = ' ';
     }
-    if (x < maze->size_x - 1 && maze->grid[y][x + 1] == ' ') {
+    if (x < maze->size_x - 1 && maze->test[y][x + 1] == ' ') {
         exploreMaze(maze, x + 1, y, length + 1);
-        maze->grid[y][x + 1] = ' ';
+        maze->test[y][x + 1] = ' ';
     }
-    if (x > 0 && maze->grid[y][x - 1] == ' ') {
+    if (x > 0 && maze->test[y][x - 1] == ' ') {
         exploreMaze(maze, x - 1, y, length + 1);
-        maze->grid[y][x - 1] = ' ';
+        maze->test[y][x - 1] = ' ';
     }
-    if (y < maze->size_y - 1 && maze->grid[y + 1][x] == ' ') {
+    if (y < maze->size_y - 1 && maze->test[y + 1][x] == ' ') {
         exploreMaze(maze, x, y + 1, length + 1);
-        maze->grid[y + 1][x] = ' ';
+        maze->test[y + 1][x] = ' ';
     }
-    return 1; /*maze->grid[y][x] = ' ';*/
 }
 
-/* TODO handling around two entrances */
+void savePath(maze* maze, int length) {
+    int i, j;
+
+    if (maze->min_route < 0) {
+        maze->min_route = length;
+
+    } else if (length < maze->min_route) {
+        maze->min_route = length;
+    }
+
+    /* Copy current shortest path into solution array */
+    for (i = 0; i < maze->size_y; i++) {
+        for (j = 0; j < maze->size_y; j++) {
+            maze->final[i][j] = maze->test[i][j];
+        }
+    }
+}
+
 void findEntrance(maze* maze) {
     int x = 0, y = 0;
 
-    /*TODO check order of operators. Can this be rewrriten in a more foolproof way? !! */
-    while (x < maze->size_x && maze->grid[0][x] != ' ') {
+    while (x < maze->size_x && maze->test[0][x] != ' ') {
         x++;
     }
 
-    /* TODO how is the maze array going to be constructed? */
-    while (y < maze->size_y && maze->grid[y][0] != ' ') {
+    while (y < maze->size_y && maze->test[y][0] != ' ') {
         y++;
     }
 
@@ -167,12 +122,24 @@ void findEntrance(maze* maze) {
     }
 }
 
+int isExit(maze* maze, int x, int y) {
+    if (y == 0 || y == maze->size_y - 1 ||
+        x == 0 || x == maze->size_x - 1) {
+        if (!(y == maze->entrance_y && x == maze->entrance_x)) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 int loadMaze(char* filename, maze* maze) {
     FILE* file;
     char* buffer = NULL;
-    int buff_size;
-    int line_len;
-    int line_index = 0;
+    int size;
+
+    int len;
+    int line = 0;
+
     int i;
 
     file = fopen(filename, "r");
@@ -180,31 +147,32 @@ int loadMaze(char* filename, maze* maze) {
         fprintf(stderr, "Cannot open \"%s\"\n", filename);
         return 0;
     }
-    getLine(&buffer, &buff_size, file);
+    getLine(&buffer, &size, file);
 
     if (!readMazeSize(maze, buffer)) {
         return 0;
     }
 
-    if (!createMazeArray(maze)) {
+    if (!initialiseMaze(maze)) {
         return 0;
     }
 
-    while ((line_len = getLine(&buffer, &buff_size, file))) {
-        if (line_len != maze->size_x) {
-            fprintf(stderr, "Invalid line length in line %d\n", line_index + 1);
+    while ((len = getLine(&buffer, &size, file))) {
+        if (len != maze->size_x) {
+            fprintf(stderr, "Invalid line length in line %d\n", line + 1);
             return 0;
         }
         for (i = 0; buffer[i] != '\0'; i++) {
-            maze->grid[line_index][i] = buffer[i];
+            maze->test[line][i] = buffer[i];
+            maze->final[line][i] = buffer[i];
         }
 
-        if (line_index > maze->size_y - 1) {
-            printf("%d ", line_index);
+        if (line > maze->size_y - 1) {
+            printf("%d ", line);
             fprintf(stderr, "Too many lines in file\n");
             return 0;
         }
-        line_index++;
+        line++;
     }
 
     free(buffer);
@@ -214,7 +182,6 @@ int loadMaze(char* filename, maze* maze) {
 }
 
 int readMazeSize(maze* maze, char* line) {
-    /* TODO what could go wrong with this? Also can we talk bmore about string literals */
     if (sscanf(line, "( %d , %d )", &maze->size_x, &maze->size_y) == 2) {
         return 1;
     }
@@ -223,15 +190,53 @@ int readMazeSize(maze* maze, char* line) {
     return 0;
 }
 
-void printMaze(maze* maze) {
-    int i, j;
+int initialiseMaze(maze* maze) {
+    maze->final = createMazeArray(maze->size_x, maze->size_y);
+    if (maze->final == NULL) {
+        return 0;
+    }
+
+    maze->test = createMazeArray(maze->size_x, maze->size_y);
+    if (maze->test == NULL) {
+        return 0;
+    }
+
+    maze->min_route = -1;
+
+    return 1;
+}
+
+char** createMazeArray(int size_x, int size_y) {
+    int i;
+    char** grid;
+
+    grid = (char**)malloc(size_y * sizeof(char*));
+
+    if (grid == NULL) {
+        fprintf(stderr, "Memory allocation error\n");
+        return NULL;
+    }
+
+    for (i = 0; i < size_y; i++) {
+        grid[i] = (char*)malloc(size_x * sizeof(char));
+
+        if (grid[i] == NULL) {
+            fprintf(stderr, "Memory allocation error\n");
+            return NULL;
+        }
+    }
+    return grid;
+}
+
+void freeMazeArray(maze* maze) {
+    int i;
 
     for (i = 0; i < maze->size_y; i++) {
-        for (j = 0; j < maze->size_x; j++) {
-            printf("%c", maze->grid[i][j]);
-        }
-        printf("\n");
+        free(maze->test[i]);
+        free(maze->final[i]);
     }
+    free(maze->test);
+    free(maze->final);
 }
 
 /* MODIFIED TO RETURN NULL CHARACTER TERMINATED LINE \n removed*/
@@ -277,64 +282,67 @@ int getLine(char** buffer, int* size, FILE* file) {
     return i;
 }
 
+void printSolution(maze* maze) {
+    int i, j;
+
+    for (i = 0; i < maze->size_y; i++) {
+        for (j = 0; j < maze->size_x; j++) {
+            printf("%c", maze->final[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 void test(void) {
     maze test_maze;
 
-    const int grid_size = 6;
+    const int test_size = 6;
 
-    int maze_exit;
     int i, j;
 
-    char test_grid[6][6] = {{'#', ' ', '#', '#', '#', '#'},
+    char test_test[6][6] = {{'#', ' ', '#', '#', '#', '#'},
                             {'#', ' ', ' ', ' ', ' ', '#'},
                             {'#', '#', '#', '#', ' ', '#'},
                             {'#', ' ', ' ', '#', ' ', '#'},
                             {'#', ' ', ' ', ' ', ' ', '#'},
                             {'#', ' ', '#', '#', '#', '#'}};
 
-    test_maze.size_x = grid_size;
-    test_maze.size_y = grid_size;
+    test_maze.size_x = test_size;
+    test_maze.size_y = test_size;
 
-    if (!createMazeArray(&test_maze)) {
+    if (!initialiseMaze(&test_maze)) {
         fprintf(stderr, "TEST ERROR\n");
         exit(EXIT_FAILURE);
     }
 
     printf("%d %d\n", test_maze.size_x, test_maze.size_y);
 
-    for (i = 0; i < grid_size; i++) {
-        for (j = 0; j < grid_size; j++) {
-            test_maze.grid[i][j] = test_grid[i][j];
-            test_maze.final_grid[i][j] = test_grid[i][j];
+    for (i = 0; i < test_size; i++) {
+        for (j = 0; j < test_size; j++) {
+            test_maze.test[i][j] = test_test[i][j];
+            test_maze.final[i][j] = test_test[i][j];
         }
     }
 
-    printMaze(&test_maze);
+    printSolution(&test_maze);
 
     findEntrance(&test_maze);
     printf("Entrance x: %d y: %d\n", test_maze.entrance_x, test_maze.entrance_y);
 
-    maze_exit = exploreMaze(&test_maze, 1, 0, 0);
-    printf("Exit status: %d\n", maze_exit);
+    exploreMaze(&test_maze, 1, 0, 0);
     printf("Check exit: %d\n", isExit(&test_maze, 1, 0));
     printf("Check exit: %d\n", isExit(&test_maze, 1, 5));
 
-    printMaze(&test_maze);
+    printSolution(&test_maze);
 
     readMazeSize(&test_maze, "(15 ,20)");
     printf("sizex: %d sizey: %d\n", test_maze.size_x, test_maze.size_y);
 
-    printf("Route length %d\n", test_maze.route_length);
+    printf("Route length %d\n", test_maze.min_route);
 
-    for (i = 0; i < grid_size; i++) {
-        for (j = 0; j < grid_size; j++) {
-            printf("%c", test_maze.final_grid[i][j]);
-        }
-        printf("\n");
-    }
+    printSolution(&test_maze);
 
     loadMaze("./Text Files/maze.txt", &test_maze);
 
-    printMaze(&test_maze);
     freeMazeArray(&test_maze);
 }
