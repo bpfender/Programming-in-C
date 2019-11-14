0cc4cf2a88d5fe34c8fdbd04bd65f7c241605e63
bpfender <benedict.pfender@gmail.com>
2019-11-14 16:43:53 +0000

    Adding testing to 72

--- Week 7/72.c
+++ Week 7/72.c
@@ -15,37 +15,40 @@
 typedef enum bool { false = 0,
                     true = 1 } bool;
 
-/* Encodes direction that tiles on the board are shifted */
+/* Encodes direction that tiles on the board are shifted 
+ */
 typedef enum swap_t { UP,
                       DOWN,
                       LEFT,
                       RIGHT } swap_t;
 
 /* Stores 8tile grid, location of free tile, the parent node and the number of
-   steps from the starting grid */
+ *  steps from the starting grid 
+ */
 typedef struct grid_t {
     int grid[SIZE][SIZE];
     int x;
     int y;
     long parent;
-    unsigned int step;
 } grid_t;
 
 /* Stores array of grid_t elements, with index of the current node and 
-   last node added to the array. "long" types used to ensure that index
-   can go high enough for many iterations */
+ * last node added to the array. "long" types used to ensure that index
+ * can go high enough for many iterations 
+ */
 typedef struct queue_t {
     grid_t node[QUEUE];
     long curr;
     long end;
 } queue_t;
 
-/* Stores list of solution steps, and variable for number
-   of steps */
-typedef struct sol_t {
-    grid_t* node[MAX_STEPS];
-    unsigned int steps;
-} sol_t;
+/* Stores list of solution steps, and variable for number of steps. Effectively
+ * implemented as a stack.
+ */
+typedef struct stack_t {
+    grid_t* stack[MAX_STEPS];
+    unsigned int top;
+} stack_t;
 
 /* ------ SOLVER FUNCTIONS ------ */
 void solve8Tile(queue_t* queue, char* s);
@@ -62,9 +65,15 @@ void initQueue(queue_t* queue, char* s);
 void loadBoard(int grid[SIZE][SIZE], char* s);
 void findFreeTile(grid_t* grid);
 
+/* ------- STACK FUNCTIONS ------ */
+void initStack(stack_t* solution);
+void push(stack_t* solution, grid_t* grid);
+grid_t* pop(stack_t* solution);
+bool isEmpty(stack_t* solution);
+
 /* ------ UTILITY & INPUT FUNCTIONS ------ */
-void loadSolution(queue_t* queue, sol_t* solution);
-void printSolution(sol_t* solution);
+void loadSolution(queue_t* queue, stack_t* solution);
+void printSolution(stack_t* solution);
 void printBoard(int grid[SIZE][SIZE]);
 bool checkInputString(char* s);
 bool isSolvable(char* s);
@@ -73,9 +82,11 @@ void test(void);
 
 int main(int argc, char* argv[]) {
     static queue_t queue;
-    sol_t solution;
+    stack_t solution;
 
-    /*test();*/
+    printf("TESTING\n");
+    test();
+    printf("MAIN\n");
 
     if (argc != 2) {
         fprintf(stderr,
@@ -95,7 +106,7 @@ int main(int argc, char* argv[]) {
     solve8Tile(&queue, argv[1]);
 
     loadSolution(&queue, &solution);
-    printf("\nPuzzle Solved in %i steps:\n\n", solution.steps);
+    printf("\nPuzzle Solved in %i steps:\n\n", solution.top - 1);
     printSolution(&solution);
 
     return 0;
@@ -155,6 +166,9 @@ bool expandNode(queue_t* queue) {
  */
 bool shiftTile(swap_t dir, queue_t* queue) {
     grid_t* parent = &queue->node[queue->curr];
+
+    /* Child is put into "temporary" index at end of queue. End index is only
+       incremented and "added" properly, when enqueue() is called */
     grid_t* child = &queue->node[queue->end + 1];
 
     /* Conditional evaluation will determine swap coordinates. "dir" will only
@@ -171,7 +185,6 @@ bool shiftTile(swap_t dir, queue_t* queue) {
     child->x = x2;
     child->y = y2;
     child->parent = queue->curr;
-    child->step = parent->step + 1;
 
     if (checkTarget(child->grid)) {
         enqueue(queue);
@@ -231,7 +244,6 @@ void initQueue(queue_t* queue, char* s) {
 
     loadBoard(queue->node[start].grid, s);
     findFreeTile(&queue->node[start]);
-    queue->node[start].step = 0;
     /* Set start to -1 for identification later */
     queue->node[start].parent = -1;
 }
@@ -274,29 +286,55 @@ void findFreeTile(grid_t* node) {
     }
 }
 
+/* ------ QUEUE FUNCTIONS ------ */
+void initStack(stack_t* solution) {
+    solution->top = 0;
+}
+
+void push(stack_t* solution, grid_t* grid) {
+    if (solution->top > MAX_STEPS) {
+        fprintf(stderr, "Stack overflow...\n");
+        exit(EXIT_FAILURE);
+    }
+    solution->stack[solution->top++] = grid;
+}
+
+grid_t* pop(stack_t* solution) {
+    if (solution->top <= 0) {
+        return NULL;
+    }
+    return solution->stack[--solution->top];
+}
+
+bool isEmpty(stack_t* solution) {
+    return !solution->top;
+}
+
 /* ------ UTILITY & INPUT FUNCTIONS ------ */
 /* Loads solution by going through parent nodes back to start grid
  */
-void loadSolution(queue_t* queue, sol_t* solution) {
-    int i;
-    long parent = queue->end;
-    /* FIXME do i need the step counter? */
-    solution->steps = queue->node[queue->end].step;
-    /* FIXME step and parent -1 one can probably be removed */
-    for (i = (int)solution->steps; i >= 0; i--) {
-        solution->node[i] = &queue->node[parent];
-        parent = queue->node[parent].parent;
+void loadSolution(queue_t* queue, stack_t* solution) {
+    long index = queue->end;
+
+    initStack(solution);
+    while (index != -1) {
+        push(solution, &queue->node[index]);
+        index = queue->node[index].parent;
     }
 }
 
-void printSolution(sol_t* solution) {
-    unsigned int i;
+void printSolution(stack_t* solution) {
+    unsigned int step = 1;
+    grid_t* node;
 
+    node = pop(solution);
     printf("Starting board:\n");
-    printBoard(solution->node[0]->grid);
-    for (i = 1; i <= solution->steps; i++) {
-        printf("Step %i:\n", i);
-        printBoard(solution->node[i]->grid);
+    printBoard(node->grid);
+
+    while ((node = pop(solution))) {
+        printf("Step %i\n\n", step);
+        printBoard(node->grid);
+        step++;
     }
 }
 
@@ -382,13 +420,12 @@ void swap(int* n1, int* n2) {
 
 void test(void) {
     long i, j;
-
-    int test_grid[SIZE][SIZE];
-    grid_t test_board;
     static queue_t test_queue;
-    sol_t test_solution;
+    grid_t test_node;
+    int test_grid[SIZE][SIZE], test_grid_2[SIZE][SIZE];
+    stack_t test_solution;
 
-    /* Testing that grid gets loaded up properly with string */
+    /* TESTING loadBoard() function */
     loadBoard(test_grid, " 12345678");
     for (i = 0; i < SIZE; i++) {
         for (j = 0; j < SIZE; j++) {
@@ -396,83 +433,130 @@ void test(void) {
         }
     }
 
-    loadBoard(test_grid, "1234 5678");
+    loadBoard(test_grid,
+              "123"
+              "4 5"
+              "678");
     assert(test_grid[0][0] == 1);
     assert(test_grid[1][1] == 0);
     assert(test_grid[2][2] == 8);
 
-    /* Testing swap of two cells */
+    /* TESTING swap() function */
     swap(&test_grid[0][0], &test_grid[2][2]);
+    swap(&test_grid[1][0], &test_grid[2][0]);
+    swap(&test_grid[1][1], &test_grid[1][2]);
     assert(test_grid[0][0] == 8);
     assert(test_grid[2][2] == 1);
+    assert(test_grid[1][0] == 6);
+    assert(test_grid[2][0] == 4);
+    assert(test_grid[1][1] == 5);
+    assert(test_grid[1][2] == 0);
 
-    printBoard(test_grid);
-
-    /* Put grid into grid_t type and check empy cell finding  */
+    /* TESTING findFreeTile() function  */
     loadBoard(test_grid, " 23415678");
-    memcpy(test_board.grid, test_grid, SIZE * SIZE * sizeof(int));
+    memcpy(test_node.grid, test_grid, SIZE * SIZE * sizeof(int));
+    findFreeTile(&test_node);
+    assert(test_node.x == 0);
+    assert(test_node.y == 0);
 
-    findFreeTile(&test_board);
-    assert(test_board.x == 0);
-    assert(test_board.y == 0);
+    loadBoard(test_grid, "14567 823");
+    memcpy(test_node.grid, test_grid, SIZE * SIZE * sizeof(int));
+    findFreeTile(&test_node);
+    assert(test_node.x == 2);
+    assert(test_node.y == 1);
 
-    printBoard(test_board.grid);
+    /* TESTING compareBoard() and checkTarget() */
+    loadBoard(test_grid, "123456 78");
+    loadBoard(test_grid_2, "123456 78");
+    assert(compareBoards(test_grid, test_grid_2) == true);
 
-    /* Check that queue initialisation works properly */
+    loadBoard(test_grid_2, "12 563478");
+    assert(compareBoards(test_grid, test_grid_2) == false);
+
+    loadBoard(test_grid, "12345678 ");
+    assert(checkTarget(test_grid) == true);
+    assert(checkTarget(test_grid_2) == false);
+
+    /* TESTING initQueue() function */
+    initQueue(&test_queue, "14567 823");
+    assert(test_queue.end == 0);
+    assert(test_queue.curr == 0);
+    assert(test_queue.node[test_queue.end].grid[0][0] == 1);
+    assert(test_queue.node[test_queue.end].grid[2][2] == 3);
+    assert(test_queue.node[test_queue.end].x == 2);
+    assert(test_queue.node[test_queue.end].y == 1);
+
+    /* TESTING shiftTile() based on init from above */
+    shiftTile(UP, &test_queue);
+    loadBoard(test_grid, "145673820");
+    assert(compareBoards(test_queue.node[1].grid, test_grid) == true);
+    assert(test_queue.node[1].parent == 0);
+    assert(test_queue.node[1].x == 2);
+    assert(test_queue.node[1].y == 2);
+    assert(test_queue.curr == 0);
+    assert(test_queue.end == 1);
+
+    shiftTile(DOWN, &test_queue);
+    loadBoard(test_grid, "140675823");
+    assert(compareBoards(test_queue.node[2].grid, test_grid) == true);
+    assert(test_queue.curr == 0);
+    /* The fact that the end index increments, indicates that enqueue() is 
+       works as expected */
+    assert(test_queue.end == 2);
+
+    /* TESTING checkUnique() function on two items loaded in queue */
+    assert(checkUnique(&test_queue, test_grid) == false);
+
+    loadBoard(test_grid, "12345 678");
+    assert(checkUnique(&test_queue, test_grid) == true);
+
+    /* Repeat UP shift. Item should not be enqeued because it's a duplicate, so
+       end index should stay the same */
+    shiftTile(UP, &test_queue);
+    assert(test_queue.end == 2);
+
+    /* Reset queue and check that it recognises target reached correctly */
+    initQueue(&test_queue, "1234567 8");
+    assert(shiftTile(LEFT, &test_queue) == true);
+    assert(test_queue.end == 1);
+
+    /* TESTING initQueue again expandNode()*/
     initQueue(&test_queue, "1234 5678");
     assert(test_queue.end == 0);
+    assert(test_queue.curr == 0);
     assert(test_queue.node[test_queue.end].grid[0][0] == 1);
     assert(test_queue.node[test_queue.end].grid[2][2] == 8);
     assert(test_queue.node[test_queue.end].x == 1);
     assert(test_queue.node[test_queue.end].y == 1);
 
-    printBoard(test_queue.node[0].grid);
-
-    /* Check that whole set of node are generated properly */
-    initQueue(&test_queue, "1234 5678");
     expandNode(&test_queue);
+    assert(test_queue.end == 4);
+    /* Having expanded the node, the next queue element should be expanded */
+    assert(test_queue.curr == 1);
 
-    /* TODO more explicit testing here. On visual inspection it work */
-    printBoard(test_queue.node[0].grid);
-    printBoard(test_queue.node[1].grid);
-    printBoard(test_queue.node[2].grid);
-    printBoard(test_queue.node[3].grid);
-    printBoard(test_queue.node[4].grid);
+    /* Left */
+    loadBoard(test_grid, "123450678");
+    assert(compareBoards(test_queue.node[1].grid, test_grid) == true);
+    /* Right */
+    loadBoard(test_grid, "123045678");
+    assert(compareBoards(test_queue.node[2].grid, test_grid) == true);
+    /* Up */
+    loadBoard(test_grid, "123475608");
+    assert(compareBoards(test_queue.node[3].grid, test_grid) == true);
+    /* Down */
+    loadBoard(test_grid, "103425678");
+    assert(compareBoards(test_queue.node[4].grid, test_grid) == true);
 
-    /* Testing of grid duplication avoidance */
-    /* TODO more explicit testing required */
-    expandNode(&test_queue);
-    for (i = 0; i <= test_queue.end; i++) {
-        printf("Board: %li\n", i);
-        assert(compareBoards(test_queue.node[0].grid, test_queue.node[i + 1].grid) == 0);
-        printBoard(test_queue.node[i].grid);
-    }
+    assert(test_queue.node[1].x == 2);
+    assert(test_queue.node[1].y == 1);
+    assert(test_queue.node[4].x == 1);
+    assert(test_queue.node[4].y == 0);
+    assert(test_queue.node[2].parent == 0);
+    assert(test_queue.curr == 1);
+    assert(test_queue.end == 4);
 
-    /* Testing of checkTarget() */
-    loadBoard(test_grid, "12345678 ");
-    assert(checkTarget(test_grid) == 1);
-
-    /* Testing of checkUnique */
-    /* TODO requires more extensive testing */
-    initQueue(&test_queue, "12345678 ");
-    loadBoard(test_grid, "12345678 ");
-    assert(checkUnique(&test_queue, test_grid) == 0);
-
-    loadBoard(test_grid, "123 45678");
-    assert(checkUnique(&test_queue, test_grid) == 1);
-
-    /* Testing of createChild() */
-    initQueue(&test_queue, "1234 5678");
-    shiftTile(DOWN, &test_queue);
-    assert(test_queue.node[1].x == 1 && test_queue.node[1].y == 0);
-    printBoard(test_board.grid);
-
-    printf("Solve START\n");
-    solve8Tile(&test_queue, "12 345678");
-    loadSolution(&test_queue, &test_solution);
-    printSolution(&test_solution);
+    
     /*
-
     assert(checkInputString("12345 678") == 1);
     assert(checkInputString("thrr 1234") == 0);
     assert(checkInputString("123") == 0);
