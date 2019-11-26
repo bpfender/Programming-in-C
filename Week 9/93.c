0681342f53c8cfa29286d3e097a40df3cbee1d4b
bpfender <benedict.pfender@gmail.com>
2019-11-26 13:52:26 +0000

    Updates of character counts

--- Week 9/93.c
+++ Week 9/93.c
@@ -1,30 +1,16 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "./file_handling.c"
 
-/* Queue struct to be used for array implementation of binary heap priority
- * queue
- * https://bradfieldcs.com/algos/trees/priority-queues-with-binary-heaps/
+/* TODO this is currently horrible. Might need to be rewritten with proper range
+ * as currently it includes characters that aren't going to appear in text e.g.
+ * NULL
  */
-typedef struct queue_t {
-    int count;
-    char c;
-    size_t end;
-} queue_t;
+#define ASCII_SIZE 127
 
-/* ------ PRIORITY QUEUE FUNCTIONS ------ */
-void initPQueue(queue_t* p_queue, node_t* node);
-void insertPQueue(queue_t* p_queue, node_t* node);
-node_t* getMin(queue_t* p_queue);
-void delMin(queue_t* p_queue);
-bool isEmpty(queue_t* p_queue);
-
-void expandPQueue(queue_t* p_queue);
-void percolateUp(queue_t* p_queue);
-void percolateDown(queue_t* p_queue);
-size_t minChildIndex(queue_t* p_queue, size_t i);
-void swapNodePtr(node_t** n1, node_t** n2);
-void unloadPQueue(queue_t* p_queue);
+void countFileCharacters(int counts[], char* filename);
+void frequencyCount(int counts[], char* s);
 
 void test(void);
 
@@ -33,5 +19,56 @@ int main(void) {
     return 0;
 }
 
+/* Count all characters in file */
+void countFileCharacters(int counts[], char* filename) {
+    FILE* file = openFile(filename);
+
+    char* buffer = NULL;
+    size_t size;
+
+    while (getLine(&buffer, &size, file)) {
+        frequencyCount(counts, buffer);
+    }
+    /* Error checking on readline 0 output */
+    if (!feof(file)) {
+        fprintf(stderr, "File reading error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    free(buffer);
+    fclose(file);
+}
+
+/* Increments character count array */
+/* TODO currently no invalid char checking */
+void frequencyCount(int counts[], char* s) {
+    size_t i = 0;
+
+    for (i = 0; s[i] != '\0'; i++) {
+        counts[(int)s[i]]++;
+    }
+}
+
 void test(void) {
+    size_t i;
+    int counts[ASCII_SIZE] = {0};
+
+    frequencyCount(counts, "aaa");
+    assert(counts['a'] == 3);
+
+    frequencyCount(counts, "Hello you are a cat");
+    assert(counts['a'] == 6);
+    assert(counts['o'] == 2);
+    assert(counts['l'] == 2);
+
+    for (i = 0; i < ASCII_SIZE; i++) {
+        counts[i] = 0;
+    }
+
+    countFileCharacters(counts, "./test.txt");
+    assert(counts['H'] == 2);
+    assert(counts['B'] == 1);
+    assert(counts['h'] == 4);
+    assert(counts['o'] == 5);
+    assert(counts['\n'] == 4);
 }
