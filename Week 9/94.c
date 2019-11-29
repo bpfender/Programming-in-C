9603a228838729660f312ed86e258e2831cbedcb
bpfender <benedict.pfender@gmail.com>
2019-11-29 20:59:31 +0000

    Need to do some work on the bracket finding code

--- Week 9/94.c
+++ Week 9/94.c
@@ -3,6 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct pos_t {
+    int* index;
+    int* stack;
+    int top;
+    int current;
+    int count;
+
+} pos_t;
+
 void test(void);
 
 int main(void) {
@@ -10,11 +19,41 @@ int main(void) {
     return 0;
 }
 
+void initBracketPos(pos_t* brackets, int leaves) {
+    brackets->index = (int*)malloc(leaves * sizeof(int));
+    brackets->stack = (int*)malloc(leaves * sizeof(int));
+    if (!(brackets->index || brackets->stack)) {
+        fprintf(stderr, "Memory allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    brackets->top = 0;
+    brackets->current = 0;
+    brackets->count = 0;
+}
+
+void getBracketPos(pos_t* brackets, char* s) {
+}
+
+int countLeaves(char* s) {
+    size_t i;
+    int leaves = 1; /* head before any brackets */
+    for (i = 0; s[i + 1] != '\0'; i++) {
+        if (s[i] == '(') {
+            leaves++;
+        }
+    }
+    return leaves;
+}
+
 char* getStrInBracket(char* s) {
     char* tmp;
-    size_t i;
-    for (i = 0; !(s[i] == '(' || s[i] == ')'); i++) {
+    size_t i = 0;
+
+    while (!(s[i] == '(' || s[i] == ')')) {
+        i++;
     }
+
     tmp = (char*)malloc((i + 1) * sizeof(char));
     memcpy(tmp, s, (i + 1) * sizeof(char));
     tmp[i] = '\0';
@@ -36,6 +75,8 @@ char* readTreeString(char* s) {
 }
 
 void test(void) {
+    size_t i;
+    pos_t brackets;
     char* str = readTreeString("HELLO");
     printf("%s\n", str);
     free(str);
@@ -46,4 +87,15 @@ void test(void) {
     str = getStrInBracket("456(");
     printf("%s\n", str);
     free(str);
+    str = getStrInBracket("*(");
+    printf("%s\n", str);
+    free(str);
+
+    initBracketPos(&brackets, 100);
+    getBracketPos(&brackets, "20(10(*)(*))(5(*)(*))");
+    /*012345678911234567892*/
+
+    for (i = 0; i < 6; i++) {
+        printf("Bracket %li: %d\n", i + 1, brackets.index[i]);
+    }
 }
