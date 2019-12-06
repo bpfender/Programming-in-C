82394852b5bf7c808498ee5c612095a6207dda6d
bpfender <benedict.pfender@gmail.com>
2019-12-06 12:35:29 +0000

    Progressing through first part

--- Week 11/mvm.c
+++ Week 11/mvm.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define AVE_KEY_LENGTH 5
+
 mvm* mvm_init(void) {
     mvm* tmp = (mvm*)calloc(1, sizeof(mvm));
     if (!tmp) {
-        ON_ERROR("Error allocating Multi-Value Map");
+        ON_ERROR("Error allocating Multi-Value Map\n");
     }
 }
 
@@ -14,9 +16,28 @@ int mvm_size(mvm* m) {
 }
 
 void mvm_insert(mvm* m, char* key, char* data) {
+    mvmcell* tmp = (mvmcell*)malloc(sizeof(mvmcell));
+    if (!tmp) {
+        ON_ERROR("Error allocating cell\n");
+    }
+    strcpy(tmp->key, key);
+    strcpy(tmp->data, data);
+    tmp->next = m->head;
+
+    m->head = tmp;
 }
 
 char* mvm_print(mvm* m) {
+    mvmcell* node = m->head;
+    char* buff = (char*)malloc(m->numkeys * AVE_KEY_LENGTH);
+    unsigned int buff_size = AVE_KEY_LENGTH;
+    if (!buff) {
+        ON_ERROR("Error allocating print buffer");
+    }
+
+    while (node) {
+                node = node->next;
+    }
 }
 
 void mvm_delete(mvm* m, char* key) {
@@ -25,6 +46,7 @@ void mvm_delete(mvm* m, char* key) {
 char* mvm_search(mvm* m, char* key) {
 }
 
+/* return null pointer for robustness to mark end of list */
 char** mvm_multisearch(mvm* m, char* key, int* n) {
 }
 
