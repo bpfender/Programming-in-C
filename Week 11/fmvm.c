73f2971a3472c358d1876890fd27dd0b74e57bc5
bpfender <benedict.pfender@gmail.com>
2019-12-10 09:18:30 +0000

    Starting on speedy search hash version

---
+++ Week 11/fmvm.c
@@ -0,0 +1,25 @@
+#include "fmvm.h"
+
+mvm* mvm_init(void) {
+}
+
+int mvm_size(mvm* m) {
+}
+
+void mvm_insert(mvm* m, char* key, char* data) {
+}
+
+char* mvm_print(mvm* m) {
+}
+
+void mvm_delete(mvm* m, char* key) {
+}
+
+char* mvm_search(mvm* m, char* key) {
+}
+
+char** mvm_multisearch(mvm* m, char* key, int* n) {
+}
+
+void mvm_free(mvm** p) {
+}
