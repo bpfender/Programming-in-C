3321a56c6ae1dbdbab53b92e3c3c347c53447f66
bpfender <benedict.pfender@gmail.com>
2020-01-06 16:27:16 +0000

    Started work on declaring functions for the interpreter

--- Week 12/interpreter.c
+++ Week 12/interpreter.c
@@ -1,1 +1,17 @@
 #include "interpreter.h"
+
+void inter_abort(prog_t* program, symbol_t* symbols) {}
+
+void inter_in2str(prog_t* program, symbol_t* symbols) {}
+
+void inter_innum(prog_t* program, symbol_t* symbols) {}
+
+void inter_jump(prog_t* program, symbol_t* symbols) {}
+
+void inter_print(prog_t* program, symbol_t* symbols) {}
+
+void inter_ifcond(prog_t* program, symbol_t* symbols) {}
+
+void inter_inc(prog_t* program, symbol_t* symbols) {}
+
+void inter_set(prog_t* program, symbol_t* symbols) {}
--- Week 12/interpreter.h
+++ Week 12/interpreter.h
@@ -1,9 +1,16 @@
 #ifndef INTERPRETER_H
 #define INTERPRETER_H
 
-#ifdef INTERP
-#endif
+#include "symbols.h"
+#include "tokenizer.h"
 
-#define void tester() 0
+void inter_abort(prog_t* program, symbol_t* symbols);
+void inter_in2str(prog_t* program, symbol_t* symbols);
+void inter_innum(prog_t* program, symbol_t* symbols);
+void inter_jump(prog_t* program, symbol_t* symbols);
+void inter_print(prog_t* program, symbol_t* symbols);
+void inter_ifcond(prog_t* program, symbol_t* symbols);
+void inter_inc(prog_t* program, symbol_t* symbols);
+void inter_set(prog_t* program, symbol_t* symbols);
 
 #endif
