a95f664631c43ea6373bd037215df91ea61728d6
bpfender <benedict.pfender@gmail.com>
2020-01-10 12:50:08 +0000

    Error fixed. To do with circular reference of heards

--- Week 12/symbols.h
+++ Week 12/symbols.h
@@ -1,7 +1,6 @@
 #ifndef SYMBOLS_H
 #define SYMBOLS_H
 
-#include "ast.h"
 #include "mvmedit.h"
 
 typedef struct symbol_t {
@@ -10,8 +9,10 @@ typedef struct symbol_t {
 } symbol_t;
 
 symbol_t* initSymbolTable(void);
+
 mvmcell* getVariable(symbol_t* symbols, char* var);
 void addVariable(symbol_t* symbols, char* var);
+
 mvmcell* getFilename(symbol_t* symbols, char* filename);
 void addFilename(symbol_t* symbols, char* filename);
 
