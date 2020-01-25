294f18f213516722cffc99734b829120ac4c1296
bpfender <benedict.pfender@gmail.com>
2020-01-25 19:07:33 +0000

    Tying together function calls

---
+++ Week 12/nal.c
@@ -0,0 +1,30 @@
+#include "parser.h"
+
+int main(int argc, char* argv[]) {
+    prog_t* program;
+    symbol_t* symbols;
+
+    if (argc != 2) {
+        fprintf(stderr, "Expected file as argument to function\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (!(program = tokenizeFile(argv[1]))) {
+        fprintf(stderr, "Error opening %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    symbols = initSymbolTable();
+    addFilename(symbols, argv[1], NULL);
+
+    if (parseFile(program, symbols)) {
+        printf("Parsed OK\n");
+    } else {
+        printf("Parse Error\n");
+    }
+
+    freeProgQueue(program);
+    freeSymbolTable(symbols);
+
+    return 0;
+}
