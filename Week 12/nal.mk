50849f1ad03d85b5498d21382f618e8370bf01f7
bpfender <benedict.pfender@gmail.com>
2020-01-25 19:17:47 +0000

    Makefile updated to build most things required

--- Week 12/nal.mk
+++ Week 12/nal.mk
@@ -7,28 +7,38 @@ BASE = nal
 BEXECS = parse interp
 SEXECS = parse_s interp_s
 DEXECS = parse_d interp_d
-EXECS = $(BEXECS) $(SEXECS) $(DEXECS)
+TEXECS = tests_s
+EXECS = $(BEXECS) $(SEXECS) $(DEXECS) $(TEXECS) extension
 
 all : $(BEXECS)
 
 # I use mvm created previously to store variables ?!
-parse : $(BASE).c $(BASE).h mvm.c mvm.h
-	$(CC) $(BASE).c mvm.c $(CFLAGS) -o $@
-parse_s : $(BASE).c $(BASE).h mvm.c mvm.h
-	$(CC) $(BASE).c mvm.c $(SFLAGS) -o $@
-parse_d : $(BASE).c $(BASE).h mvm.c mvm.h
-	$(CC) $(BASE).c mvm.c $(DFLAGS) -o $@
+parse : $(BASE).c
+	$(CC) $(BASE).c tokenizer.c symbols.c parser.c error.c $(CFLAGS) -o $@
+parse_s : $(BASE).c
+	$(CC) $(BASE).c tokenizer.c symbols.c parser.c error.c $(SFLAGS) -o $@
+parse_d : $(BASE).c 
+	$(CC) $(BASE).c tokenizer.c symbols.c parser.c error.c $(DFLAGS) -o $@
 
 # I #define INTERP (via this makefile) so that only the extra code for
 # the interpreter is inside #ifdef INTERP / #endif 'brackets'
-interp : $(BASE).c $(BASE).h mvm.c mvm.h
-	$(CC) $(BASE).c mvm.c $(CFLAGS) -o $@ -DINTERP
-interp_s : $(BASE).c $(BASE).h mvm.c mvm.h
-	$(CC) $(BASE).c mvm.c $(SFLAGS) -o $@ -DINTERP
-interp_d : $(BASE).c $(BASE).h mvm.c mvm.h
-	$(CC) $(BASE).c mvm.c $(DFLAGS) -o $@ -DINTERP
+interp : $(BASE).c 
+	$(CC) $(BASE).c tokenizer.c symbols.c parser.c interpreter.c error.c $(CFLAGS) -o $@ -DINTERP
+interp_s : $(BASE).c 
+	$(CC) $(BASE).c tokenizer.c symbols.c parser.c interpreter.c error.c $(SFLAGS) -o $@ -DINTERP
+interp_d : $(BASE).c 
+	$(CC) $(BASE).c tokenizer.c symbols.c parser.c interpreter.c error.c $(DFLAGS) -o $@ -DINTERP
+
+extension: $(BASE).c 
+	$(CC) $(BASE).c tokenizer.c symbols.c parser.c error.c $(CFLAGS) -o $@ -DEXTENSION
+
+tests_s: test.c
+	$(CC) test.c tokenizer.c symbols.c parser.c error.c $(DFLAGS) -o $@
 
 test : testparse testinterp
+
+testfile: tests_s
+	./tests_s
 
 testparse : parse_s parse_d
 	./parse_s test1.$(BASE)
