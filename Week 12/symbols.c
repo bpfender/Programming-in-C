2171d36d790a5c26d12693f8a2ece52ee17e133b
bpfender <benedict.pfender@gmail.com>
2020-01-26 01:15:20 +0000

    Symbols basically ready. Addfilename is a bit horrible due to empty value

--- Week 12/symbols.c
+++ Week 12/symbols.c
@@ -1,15 +1,8 @@
-#include "symbols.h"
 #include <stdio.h>
 #include <string.h>
 
-/* Average word length in english is 4.7 chars. With mvm_print() returning 2 
-strings this can just be rounded to 10 */
-#define AVE_CHARS 10
-#define FRMT_CHARS strlen("[]() ")
+#include "symbols.h"
 
-#define PRNT_STR_LEN AVE_CHARS + FRMT_CHARS
-#define MULTI_SEARCH_LEN 5
-/* Universal resizing factor */
 #define FACTOR 2
 
 /* ------ SYMBOL TABLE HANDLING ------ */
@@ -35,11 +28,9 @@ mvmcell* getVariable(symbol_t* symbols, char* var) {
     return mvm_search(symbols->vars, var);
 }
 
-/* FIXME this needs to be modified */
 void updateVariable(symbol_t* symbols, char* var, void* val) {
     mvmcell* cell = mvm_search(symbols->vars, var);
     if (cell) {
-        /* QUESTION Ok to call free on nulled data? */
         if (cell->data) {
             free(cell->data);
         }
@@ -49,25 +40,20 @@ void updateVariable(symbol_t* symbols, char* var, void* val) {
     }
 }
 
-/* FIXME This is not pretty at the moment */
-void addVariable(symbol_t* symbols, char* var) {
-    if (!mvm_search(symbols->vars, var)) {
-        mvm_insert(symbols->vars, var, NULL);
-    }
-}
-
 /* ------- FILENAME HANDLING ------- */
 mvmcell* getFilename(symbol_t* symbols, char* filename) {
     return mvm_search(symbols->files, filename);
 }
 
+/* This was defined like this in the hopes of doing multi-pass lexical and 
+ * syntactic analysis. Void* was supposed to point to beginning of program queue, but
+ * it doesn't actually matter what the value is for current implementation
+*/
 void addFilename(symbol_t* symbols, char* filename, void* prog) {
     mvm_insert(symbols->files, filename, prog);
 }
 
 /* ------- MVM UPDATE FUNCTIONS ------ */
-/* Initialises mvm ADT and zeros all values
- */
 mvm* mvm_init(void) {
     mvm* tmp = (mvm*)allocHandler(NULL, 1, sizeof(mvm));
     tmp->head = NULL;
@@ -76,14 +62,6 @@ mvm* mvm_init(void) {
     return tmp;
 }
 
-/* Returns 0 if m is null, or the "numkeys" value stored in m 
- */
-int mvm_size(mvm* m) {
-    return m ? m->numkeys : 0;
-}
-
-/* Insert element at head of linked list stored in mvm ADT. Does nothing when
- * given an invalid input */
 void mvm_insert(mvm* m, char* key, void* data) {
     if (m && key) {
         mvmcell* node = mvmcell_init(key, data);
@@ -94,8 +72,6 @@ void mvm_insert(mvm* m, char* key, void* data) {
     }
 }
 
-/* Returns pointer to cell where key is stored
- */
 mvmcell* mvm_search(mvm* m, char* key) {
     if (m && key) {
         return mvm_findKey(m->head, key);
@@ -112,9 +88,7 @@ void mvm_free(mvm** p) {
 }
 
 /* ------ HELPER FUNCTIONS ------ */
-/* Returns pointer to mvmcell node where key is found. If the key doesn't exist
- * returns NULL.
- */
+
 mvmcell* mvm_findKey(mvmcell* head, char* key) {
     mvmcell* node = head;
 
@@ -127,11 +101,6 @@ mvmcell* mvm_findKey(mvmcell* head, char* key) {
     return NULL;
 }
 
-/* Allocates memory for mvmcell that will be added to LL and populates values.
- * next ptr doesn't have to be zeroed because elements are always added to head
- * of mvm list, which is initialised with NULL. Data is expected as an already
- * malloced piece of data.
- */
 mvmcell* mvmcell_init(char* key, void* data) {
     mvmcell* node = (mvmcell*)allocHandler(NULL, 1, sizeof(mvmcell));
 
@@ -143,11 +112,7 @@ mvmcell* mvmcell_init(char* key, void* data) {
     return node;
 }
 
-/* Recursive helper function to delete element from linked list. Currently just
- * deletes the first item found to conform to testing in "testmvm.c". Function
- * returns address of next node to previous call, in turn removing the specified
- * node. 
- */
+
 mvmcell* mvmcell_deleteHelper(mvmcell* node, char* key, mvm* m) {
     mvmcell* tmp;
 
@@ -164,9 +129,6 @@ mvmcell* mvmcell_deleteHelper(mvmcell* node, char* key, mvm* m) {
     }
 }
 
-/* Recursive function to unload linked list. Still not completely sure if it's
- * better to do this recursively or with a loop?
- */
 void mvmcell_unloadList(mvmcell* node) {
     if (!node) {
         return;
@@ -175,8 +137,6 @@ void mvmcell_unloadList(mvmcell* node) {
     mvmcell_unloadNode(node);
 }
 
-/* Unload node helper that frees all parts of the mvmcell LL node
- */
 void mvmcell_unloadNode(mvmcell* node) {
     if (node->data) {
         free(node->data);
@@ -185,10 +145,6 @@ void mvmcell_unloadNode(mvmcell* node) {
     free(node);
 }
 
-/* Wrote a generic malloc/realloc function because the same structure was
- * repeating itself multiple times and I wanted to play with void*. Requires
- * ptr = NULL for initial malloc, and ptr value for resizing an existing block
- */
 void* allocHandler(void* ptr, size_t nmemb, size_t size) {
     void* tmp = realloc(ptr, nmemb * size);
     if (!tmp) {
@@ -197,39 +153,3 @@ void* allocHandler(void* ptr, size_t nmemb, size_t size) {
     return tmp;
 }
 
-/* Writes string into buffer that is dynamically resized as required. Would have
- * liked to use strncat() for easier buffer resizing but I believe this is a GNU
- * only extension. Buffer is initially sized based on average word lengths and
- * ()[] string, though string lengths will of course vary depending on the data
- * stored. 
- */
-char* mvm_print(mvm* m) {
-    if (m) {
-        size_t buffer_size = PRNT_STR_LEN * m->numkeys;
-        char* buffer = (char*)allocHandler(NULL, buffer_size, sizeof(char));
-
-        mvmcell* node = m->head;
-        size_t curr_index = 0;
-        size_t next_index = 0;
-
-        /* Loop first checks length of string to be appended to ensure it can 
-        fit the buffer. If required, buffer is expanded and string is then added
-        to the buffer. Loop continues to last node in linked list */
-        while (node) {
-            next_index += strlen(node->key) + 3;
-
-            /* Check with "+ 1" to ensure there is space for NUll terminator if 
-            this is the final appended string. Resize buffer if required */
-            if (next_index + 1 >= buffer_size) {
-                buffer_size = next_index * FACTOR;
-                buffer = (char*)allocHandler(buffer, buffer_size, sizeof(char));
-            }
-
-            sprintf(buffer + curr_index, "[%s] ", node->key);
-            curr_index = next_index;
-            node = node->next;
-        }
-        return buffer;
-    }
-    return NULL;
-}
--- Week 12/symbols.h
+++ Week 12/symbols.h
@@ -39,8 +39,8 @@ void freeSymbolTable(symbol_t* symbols);
  */
 mvmcell* getVariable(symbol_t* symbols, char* var);
 
-/* Adds key value to symbol table */
-void addVariable(symbol_t* symbols, char* var);
+/* Checks whether variable already exists and updates value if it does. If not
+ * add the variable to the symbol table */
 void updateVariable(symbol_t* symbols, char* var, void* val);
 
 /* ------ FILENAME HANDLING ------ */
@@ -56,27 +56,56 @@ void addFilename(symbol_t* symbols, char* filename, void* prog);
 
 
 /* ------- MVM UPDATED FUNCTIONS ------- */
+/* Initialises mvm ADT and zeros all values
+ */
 mvm* mvm_init(void);
-/* Number of key/value pairs stored */
-int mvm_size(mvm* m);
-/* Insert one key/value pair */
+
+/* Insert element at head of linked list stored in mvm ADT. Does nothing when
+ * given an invalid input 
+ */
 void mvm_insert(mvm* m, char* key, void* data);
-/* Remove one key/value */
-void mvm_delete(mvm* m, char* key);
-/* Return the corresponding value for a key */
+
+/* Returns pointer to cell where key is stored or NULL if it doesn't exist
+ */
 mvmcell* mvm_search(mvm* m, char* key);
-/* Free & set p to NULL */
+
+/* Free & set p to NULL 
+ */
 void mvm_free(mvm** p);
 
-char* mvm_print(mvm* m);
-
 /* ------ HELPER FUNCTION DECLARATIONS ------ */
-/* These functions do not need to be exposed to the user */
+/* Returns pointer to mvmcell node where key is found. If the key doesn't exist
+ * returns NULL.
+ */
 mvmcell* mvm_findKey(mvmcell* head, char* key);
+
+/* Allocates memory for mvmcell that will be added to LL and populates values.
+ * next ptr doesn't have to be zeroed because elements are always added to head
+ * of mvm list, which is initialised with NULL. Data is expected as an already
+ * malloced piece of data.
+ */
 mvmcell* mvmcell_init(char* key, void* data);
+
+/* Recursive helper function to delete element from linked list. Currently just
+ * deletes the first item found to conform to testing in "testmvm.c". Function
+ * returns address of next node to previous call, in turn removing the specified
+ * node. 
+ */
 mvmcell* mvmcell_deleteHelper(mvmcell* node, char* key, mvm* m);
+
+/* Recursive function to unload linked list. Still not completely sure if it's
+ * better to do this recursively or with a loop?
+ */
 void mvmcell_unloadList(mvmcell* node);
+
+/* Unload node helper that frees all parts of the mvmcell LL node
+ */
 void mvmcell_unloadNode(mvmcell* node);
+
+/* Wrote a generic malloc/realloc function because the same structure was
+ * repeating itself multiple times and I wanted to play with void*. Requires
+ * ptr = NULL for initial malloc, and ptr value for resizing an existing block
+ */
 void* allocHandler(void* ptr, size_t nmemb, size_t size);
 
 #endif
